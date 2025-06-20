0eca483fb4607958fd1ea06c8648e5d54343a391
Mahadevan Reji <rmahadevan574@gmail.com>
2025-06-20 20:28:04 +0530

    Refactor environment variable checks and improve startup logging

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const { handleMessage } = require("./modules/message");
 
-
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -18,5 +17,23 @@ client.on("ready", () => {
 
 client.login(process.env.BOT_TOKEN);
 
-console.log("Bot is starting up...😎");
+const checkEnvVariables = () => {
+  const botToken = process.env.BOT_TOKEN;
+  const tenApiKey = process.env.TENOR_API_KEY;
+  const groqApiKey = process.env.GROQ_API_KEY;
 
+  if (!botToken)
+    console.log("Error: BOT_TOKEN is not set in the environment variables.");
+
+  if (!tenApiKey)
+    console.log(
+      "Error: TENOR_API_KEY is not set in the environment variables."
+    );
+
+  if (!groqApiKey)
+    console.log("Error: GROQ_API_KEY is not set in the environment variables.");
+};
+
+checkEnvVariables();
+
+console.log("Bot is starting up...😎");
